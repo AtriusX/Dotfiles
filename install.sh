8b95ecc0450ff722146603165829b12e57f4a3c1
Atri <atriusedm@gmail.com>
2025-10-23 19:23:55 -0400

    chore: Add better resiliancy for  the script

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/bash
 
+# Check for curl installation
+if [[ -z "$(type -t curl)" ]]; then
+    echo "Cannot continue because curl is not installed. Please install curl and re-run this script."
+    return 1
+fi
+# Get OS ID from /etc/os-release
 OS_TYPE=$(grep -oP '^ID=\K[^"]+' /etc/os-release)
 
 # shellcheck source=/dev/null
 source install_scripts/install_shell.sh
 # shellcheck source=/dev/null
 source install_scripts/install_stow.sh
-
+# Install shell environment
 install_shell "$OS_TYPE"
-
+# Install and apply stow
 install_stow "$OS_TYPE"
-
-stow .
+stow_configs
--- install_scripts/install_stow.sh
+++ install_scripts/install_stow.sh
@@ -27,3 +27,23 @@ install_stow() {
 
     echo "Stow installation complete."
 }
+
+stow_configs() {
+    if stow .; then
+        echo "Stow operation completed successfully."
+    fi
+
+    echo "Stow encountered an error during initial stow."
+    echo "You can force the operation by typing 'force', otherwise hit enter to skip and you can stow manually later."
+    echo "This will cause any local changes to be lost, so only do this if you are sure you want to overwrite your local configs!"
+    
+    read -r force
+
+    if [ "${force,,}" == "force" ]; then
+        echo "Forcing stow operation..."
+        stow --adopt .
+        git reset --hard origin/main
+    fi
+
+    echo "Stow operation complete. Configurations have been overwritten."
+}
